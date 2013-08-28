492c9c64ee9c4ad8e8e3f2222049904a90c3d195
ram <ramkumar013@live.com>
2013-08-28 15:51:47 +0530

    skeleton chcek-in

---
+++ ECMS.Core/IRouteManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ECMS.Core
+{
+    interface IRouteManager
+    {
+        List<Route> GetAllRoutes(int siteId_);
+    }
+}

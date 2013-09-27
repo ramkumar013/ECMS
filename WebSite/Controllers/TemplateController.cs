89a57a1cec83f9c4c5586d27abfecf3788194c36
ram <ramkumar013@live.com>
2013-09-27 20:56:05 +0530

    return json to view and retrieving value in view example

--- WebSite/Controllers/TemplateController.cs
+++ WebSite/Controllers/TemplateController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using WebSite.App_Code;
 
 namespace WebSite.Controllers
@@ -19,8 +20,9 @@ namespace WebSite.Controllers
 
         public ActionResult Compose()
         {
-            dynamic model = new { FirstName = "Vishal", LastName = "Sharma", FlatNo = "2104A", FirstName1 = "Vishal", LastName1 = "Sharma", FlatNo1 = "2104A", FirstName2 = "Vishal", LastName2 = "Sharma", FlatNo2 = "2104A", FirstName3 = "Vishal", LastName3 = "Sharma", FlatNo3 = "2104A", FirstName4 = "Vishal", LastName4 = "Sharma", FlatNo4 = "2104A", FirstName5 = "Vishal", LastName5 = "Sharma", FlatNo5 = "2104A", FirstName6 = "Vishal", LastName6 = "Sharma", FlatNo6 = "2104A" };
-            return View("~/Views/" + this.CurrentUrl.SiteId + this.CurrentUrl.View + ".cshtml", new MvcHelpers.ReflectionDynamicObject() { RealObject = model });
+            var model = new JavaScriptSerializer().Deserialize("{ \"FirstName\" : \"Vishal\", \"LastName\" : \"Sharma\", \"FlatNo\" : \"2104A\", \"Models\" : [{\"Make\":\"Maruti\",\"Model\":\"Alto\"},{\"Make\":\"Ranault\",\"Model\":\"Duster\"}]  }", typeof(System.Object));
+            //return View("~/Views/" + this.CurrentUrl.SiteId + this.CurrentUrl.View + ".cshtml", new MvcHelpers.ReflectionDynamicObject() { RealObject = model });
+            return View("~/Views/" + this.CurrentUrl.SiteId + this.CurrentUrl.View + ".cshtml", model);
         }
     }
 }

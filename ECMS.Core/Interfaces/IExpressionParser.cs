c25cd5b0ec7a6eadab2b3125c12de4c3055f0280
ram <ramkumar013@live.com>
2013-10-05 23:04:09 +0530

    ExpressionParsing-WIP

---
+++ ECMS.Core/Interfaces/IExpressionParser.cs
@@ -0,0 +1,18 @@
+﻿using ECMS.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ECMS.Core.Interfaces
+{
+    public interface IExpressionParser
+    {
+        string ParseExpression(StringBuilder contentToParse_);
+        HttpContextBase CurrentContext { get; set; }
+        ValidUrl CurrentUrl { get; set; }
+        bool ParserResult { get; set; }
+    }
+}

af6897db31abeda3c7ff4e1ec2bba808323fc99d
ram <ramkumar013@live.com>
2013-10-20 23:50:48 +0530

    Putting Design in Template - WIP

--- ECMS.Services/ContentRepository/FileSystemRepository.cs
+++ ECMS.Services/ContentRepository/FileSystemRepository.cs
@@ -7,16 +7,41 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using ECMS.Core.Framework;
+using System.IO;
+using CsvHelper;
 
 namespace ECMS.Services.ContentRepository
 {
     public class FileSystemRepository : ContentRepositoryBase
     {
+        public static List<ContentItemHead> ContentHeadList = null;
+        public static Dictionary<Guid, dynamic> ContentBodyList = null;
+
+        static FileSystemRepository()
+        {
+            using (StreamReader streamReader = new StreamReader(@"J:\MyProjects\ecms\WebSite\App_Data\1\default-template.et"))
+            {
+                var csv = new CsvReader(streamReader);
+                ContentHeadList = csv.GetRecords<ContentItemHead>().ToList<ContentItemHead>();
+            }
+
+            using (StreamReader streamReader = new StreamReader(@"J:\MyProjects\ecms\WebSite\App_Data\1\content.ect"))
+            {
+                var csv = new CsvReader(streamReader);
+                ContentBodyList = new Dictionary<Guid, dynamic>();
+                while (csv.Read())
+                {
+                    ContentBodyList[Guid.Parse(csv.GetField("UrlId"))] = csv.GetRecord(typeof(object));
+                }
+            }
+        }
+
         public override ContentItem GetById(ValidUrl url_)
         {
             ContentItem item = new ContentItem();
             item.Url = url_;
-            item.Body = JsonConvert.DeserializeObject("{ \"FirstName\" : \"Vishal\", \"LastName\" : \"Sharma\", \"FlatNo\" : \"2104A\", \"Models\" : [{\"Make\":\"Maruti\",\"Model\":\"Alto\", \"Year\":\"@DateTime.Now.Year.ToString()\"},{\"Make\":\"Ranault\",\"Model\":\"Duster\", \"Year\":\"@DateTime.Now.Year.ToString()\"}]  }");
+            //item.Body = JsonConvert.DeserializeObject("{ \"FirstName\" : \"Vishal\", \"LastName\" : \"Sharma\", \"FlatNo\" : \"2104A\", \"Models\" : [{\"Make\":\"Maruti\",\"Model\":\"Alto\", \"Year\":\"@DateTime.Now.Year.ToString()\"},{\"Make\":\"Ranault\",\"Model\":\"Duster\", \"Year\":\"@DateTime.Now.Year.ToString()\"}]  }");
+            item.Body = ContentBodyList[url_.Id];
             item.Head = GetHeadContentByViewName(url_);
             return item;
         }
@@ -39,7 +64,8 @@ namespace ECMS.Services.ContentRepository
         public override ContentItemHead GetHeadContentByViewName(ValidUrl url_)
         {
             //TODO: dummy implementation to be change
-            ContentItemHead itemhead = JsonConvert.DeserializeObject<ContentItemHead>("{ \"Title\" : \"page title\", \"KeyWords\" : \"page keywords\", \"Description\" : \"test page\"}");
+            //ContentItemHead itemhead = JsonConvert.DeserializeObject<ContentItemHead>("{ \"Title\" : \"page title\", \"KeyWords\" : \"page keywords\", \"Description\" : \"test page\"}");
+            ContentItemHead itemhead = ContentHeadList[0];
             return itemhead;
         }
     }

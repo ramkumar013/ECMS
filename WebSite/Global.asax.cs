001215bd573e5e160ebebd8bf217e5a98e75e903
ram <ramkumar013@live.com>
2013-12-01 15:49:57 +0530

    error handling working fine...

--- WebSite/Global.asax.cs
+++ WebSite/Global.asax.cs
@@ -67,40 +67,40 @@ namespace WebApp
                 DependencyManager.Logger.Log(info);
             }
 
-            //var ex = Server.GetLastError();
-            //var controller = new ErrorController();
-            //var routeData = new RouteData();
-            //var action = "Index";
-            //if (ex != null)
-            //{
-            //    LogEventInfo info = new LogEventInfo(LogLevel.Error, ECMSSettings.DEFAULT_LOGGER, ex.ToString());
-            //    DependencyManager.Logger.Log(info);
-            //}
-            //if (ex is HttpException)
-            //{
-            //    var httpEx = ex as HttpException;
+            var ex = Server.GetLastError();
+            var controller = new ErrorController();
+            var routeData = new RouteData();
+            var action = "Index";
+            if (ex != null)
+            {
+                LogEventInfo info = new LogEventInfo(LogLevel.Error, ECMSSettings.DEFAULT_LOGGER, ex.ToString());
+                DependencyManager.Logger.Log(info);
+            }
+            if (ex is HttpException)
+            {
+                var httpEx = ex as HttpException;
 
-            //    switch (httpEx.GetHttpCode())
-            //    {
-            //        case 404:
-            //            action = "NotFound";
-            //            break;
-            //        default:
-            //            action = "Index";
-            //            break;
-            //    }
-            //}
+                switch (httpEx.GetHttpCode())
+                {
+                    case 404:
+                        action = "NotFound";
+                        break;
+                    default:
+                        action = "Index";
+                        break;
+                }
+            }
 
-            //httpContext.ClearError();
-            //httpContext.Response.Clear();
-            //httpContext.Response.StatusCode = ex is HttpException ? ((HttpException)ex).GetHttpCode() : 500;
-            //httpContext.Response.TrySkipIisCustomErrors = true;
+            httpContext.ClearError();
+            httpContext.Response.Clear();
+            httpContext.Response.StatusCode = ex is HttpException ? ((HttpException)ex).GetHttpCode() : 500;
+            httpContext.Response.TrySkipIisCustomErrors = true;
 
-            //routeData.Values["controller"] = "Error";
-            //routeData.Values["action"] = action;
+            routeData.Values["controller"] = "Error";
+            routeData.Values["action"] = action;
 
-            //controller.ViewData.Model = new HandleErrorInfo(ex, currentController, currentAction);
-            //((IController)controller).Execute(new RequestContext(new HttpContextWrapper(httpContext), routeData));
+            controller.ViewData.Model = new HandleErrorInfo(ex, currentController, currentAction);
+            ((IController)controller).Execute(new RequestContext(new HttpContextWrapper(httpContext), routeData));
         }
     }
 }

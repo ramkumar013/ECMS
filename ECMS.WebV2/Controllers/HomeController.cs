a41503ca6289e153d2346dfa06b515cbb22419a7
ram <ramkumar013@live.com>
2013-12-28 21:30:22 +0530

    Using Shared layout on admin views only.. removed default home folder from views

--- ECMS.WebV2/Controllers/HomeController.cs
+++ ECMS.WebV2/Controllers/HomeController.cs
@@ -10,21 +10,7 @@ namespace ECMS.WebV2.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(@"~/Views/Admin/Index.cshtml");
         }
     }
 }

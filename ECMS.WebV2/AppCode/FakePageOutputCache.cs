6e654667bc437003fe73fad329a34a0ef1563dfc
ram <ramkumar013@live.com>
2013-12-30 00:30:50 +0530

    Configurable FakePageOutputCache

---
+++ ECMS.WebV2/AppCode/FakePageOutputCache.cs
@@ -0,0 +1,29 @@
+﻿
+namespace ECMS.WebV2.AppCode
+{
+    
+    public class FakePageOutputCache : System.Web.Caching.OutputCacheProvider
+    {
+
+        public override object Add(string key, object entry, System.DateTime utcExpiry)
+        {
+            return entry;
+        }
+
+        public override object Get(string key)
+        {
+            return null;
+        }
+
+
+        public override void Remove(string key)
+        {
+        }
+
+
+        public override void Set(string key, object entry, System.DateTime utcExpiry)
+        {
+        }
+
+    }
+}
